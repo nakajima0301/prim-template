4a10e19c226acec2e1fe2cc985707d839a85362d
Kazuki Nakajima <nakajima.k0301@gmail.com>
2020-08-05 07:40:34 +0900

    [UP] Add command line options

--- engine.sh
+++ engine.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-TEMPLATE='template.conf'
-WORKERS=10
+CMDNAME=`basename $0`
 
-export TAG="test.tag"
-export WORKER_NUMBER="0"
+# Default value
+TEMPLATE="template.conf"
+QUANTITY=1
 
-for i in $(seq 0 `expr $WORKERS - 1`); do
-    export WORKER_NUMBER="$i"
-    envsubst < ${TEMPLATE} > ${WORKER_NUMBER}.conf
+function usage() {
+    echo "Usage: $CMDNAME [-f filename]" 1>&2
+    exit 1
+}
+
+while getopts hf: OPT
+do
+    case $OPT in
+        "f" ) flag_f="true"; value_f="$OPTARG";;
+        "h" ) usage;;
+        * ) usage
+    esac
+done
+
+if [[ $flag_f == "true" ]]; then
+    TEMPLATE=$value_f
+fi
+
+if [[ -e $TEMPLATE ]]; then
+    echo "Using template: $TEMPLATE"
+else
+    echo "$TEMPLATE: No such file" 1>&2
+    exit 1
+fi
+
+for i in $(seq 0 `expr $QUANTITY - 1`); do
+    envsubst < ${TEMPLATE} > ${i}.out
 done
 
